69e0e51ca978014c49413250f403f50cd48e17fa
Leonid <parshentsevla@gmail.com>
2024-11-15 15:36:11 +0300

    Тема 3. Решение задач

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathSlices = path.split('.')
 
+    return (obj) => {
+        for (const slice of pathSlices) {
+            if (!obj.hasOwnProperty(slice)) {
+                return
+            }
+
+            obj = obj?.[slice]
+        }
+
+        return obj
+    }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === 0) {
+        return ''
+    }
 
+    const arr = string.split('')
+    let l = 0
+
+    for (let r = 1; r < arr.length; r++) {
+        if (arr[l] !== arr[r]) {
+            l = r
+        } else {
+            while (r + 1 - l > size) {
+                arr[l] = -1
+                l++
+            }
+        }
+    }
+
+    return arr.filter((char) => char !== -1).join('')
 }
